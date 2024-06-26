43199e971ad7bee1808cab9040427bc2fec2761e
Federico Aguzzi <62149513+f-aguzzi@users.noreply.github.com>
2024-06-26 17:42:44 +0200

    fix: please work. I need this to work.

--- src/chemfusekit/__init__.py
+++ src/chemfusekit/__init__.py
@@ -2,13 +2,3 @@
 from beartype import BeartypeConf
 from beartype.claw import beartype_this_package
 beartype_this_package(conf=BeartypeConf(violation_type=TypeError))
-
-from . import _base
-from . import df
-from . import knn
-from . import lda
-from . import lr
-from . import pca
-from . import plsda
-from . import svm
-from . import _utils
