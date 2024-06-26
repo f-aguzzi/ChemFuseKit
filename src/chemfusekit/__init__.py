e17bfecb5f8b93d4bfe7d0f1a2a760de6077b295
Federico Aguzzi <62149513+f-aguzzi@users.noreply.github.com>
2024-06-26 17:50:18 +0200

    Revert "fix: please work. I need this to work."
    This reverts commit 43199e971ad7bee1808cab9040427bc2fec2761e.

--- src/chemfusekit/__init__.py
+++ src/chemfusekit/__init__.py
@@ -2,3 +2,13 @@
 from beartype import BeartypeConf
 from beartype.claw import beartype_this_package
 beartype_this_package(conf=BeartypeConf(violation_type=TypeError))
+
+from . import _base
+from . import df
+from . import knn
+from . import lda
+from . import lr
+from . import pca
+from . import plsda
+from . import svm
+from . import _utils
