8143f74698cabd77e70e9f11c754b74ef3d677d5
Federico Aguzzi <62149513+f-aguzzi@users.noreply.github.com>
2024-06-26 12:53:46 +0200

    fix: base-level imports

--- src/chemfusekit/__init__.py
+++ src/chemfusekit/__init__.py
@@ -3,12 +3,12 @@ from beartype import BeartypeConf
 from beartype.claw import beartype_this_package
 beartype_this_package(conf=BeartypeConf(violation_type=TypeError))
 
-import base
-import df
-import knn
-import lda
-import lr
-import pca
-import plsda
-import svm
-import utils
+from . import base
+from . import df
+from . import knn
+from . import lda
+from . import lr
+from . import pca
+from . import plsda
+from . import svm
+from . import utils
