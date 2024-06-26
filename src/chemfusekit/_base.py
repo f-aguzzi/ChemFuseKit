4e9d3b9eed9c5fc5f208d5fab82a84225cf03b6b
Federico Aguzzi <62149513+f-aguzzi@users.noreply.github.com>
2024-06-26 17:30:25 +0200

    fix(base): remove bug-causing BaseEstimator check

--- src/chemfusekit/_base.py
+++ src/chemfusekit/_base.py
@@ -6,8 +6,6 @@ import pandas as pd
 import numpy as np
 import joblib
 
-from sklearn.base import BaseEstimator
-
 from ._utils import GraphMode
 
 
@@ -108,7 +106,7 @@ class BaseActionClass(ABC):
     def __init__(self, settings: BaseSettings, data: BaseDataModel):
         self.settings = settings
         self.data = data
-        self.model: BaseEstimator | None = None
+        self.model = None
 
     @abstractmethod
     def train(self):
@@ -137,8 +135,6 @@ class BaseActionClass(ABC):
             model = joblib.load(import_path)
         except Exception as exc:
             raise ImportError("The file you tried importing is not a valid Python object!") from exc
-        if not isinstance(model, BaseEstimator):
-            raise ImportError("The file you tried importing is not a sklearn model!")
         self.model = model
 
     def export_model(self, export_path: str):
